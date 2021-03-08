c765bfb03e82c153ad4936b0cead74b686d0e659
Ellie Cho <elliecho5@gmail.com>
2021-03-07 23:10:29 -0500

    Initial Commit

---
+++ tipcalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tipcalculator
+//
+//  Created by Ellie Cho on 3/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
